8e1e51900f6bf7c7aba7208c116b18565757bdaa
PinewoodPip <53646914+PinewoodPip@users.noreply.github.com>
2020-05-04 15:20:34 +0200

    Updates

---
+++ precache-manifest.8a50f56b3a06757370c5a3c47e9fee4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a95f59838f7ac55769b24853bd6d8f7",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "a4999c2bb8d9e0e1b488",
+    "url": "/bingo/static/css/main.e090ae4e.chunk.css"
+  },
+  {
+    "revision": "ddf586dd852724d8be43",
+    "url": "/bingo/static/js/2.ea4b240e.chunk.js"
+  },
+  {
+    "revision": "244db3309ebea917954879828b3d05cb",
+    "url": "/bingo/static/js/2.ea4b240e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4999c2bb8d9e0e1b488",
+    "url": "/bingo/static/js/main.df43fe34.chunk.js"
+  },
+  {
+    "revision": "e60f173697a37915a237",
+    "url": "/bingo/static/js/runtime-main.0401bbb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bingo/static/media/logo.5d5d9eef.svg"
+  }
+]);
